5ef2b94502f01420295fd6f7bd7e42f0d1b48631
Aslam <iupin5212@gmail.com>
2021-03-26 23:15:37 +0700

    H2H feature

--- src/H2H.php
+++ src/H2H.php
@@ -6,9 +6,9 @@ use Aslam\Bni\Bni;
 
 class H2H extends Bni
 {
-    public function __construct()
+    public function __construct($token)
     {
-        parent::__construct();
+        parent::__construct($token);
     }
 
     /**
@@ -21,5 +21,89 @@ class H2H extends Bni
     {
         $requestUrl = $this->apiUrl . '/H2H/v2/getbalance';
         return $this->sendRequest('POST', $requestUrl, compact('accountNo'));
+    }
+
+    /**
+     * getInHouseInquiry
+     *
+     * @param  string $accountNo
+     * @return \Aslam\Bni\Response
+     */
+    public function getInHouseInquiry(string $accountNo)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/getinhouseinquiry';
+        return $this->sendRequest('POST', $requestUrl, compact('accountNo'));
+    }
+
+    /**
+     * doPayment
+     *
+     * @param  array $fields
+     * @return \Aslam\Bni\Response
+     */
+    public function doPayment(array $fields)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/dopayment';
+        return $this->sendRequest('POST', $requestUrl, $fields);
+    }
+
+    /**
+     * getPaymentStatus
+     *
+     * @param  string $customerReferenceNumber
+     * @return \Aslam\Bni\Response
+     */
+    public function getPaymentStatus(string $customerReferenceNumber)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/getpaymentstatus';
+        return $this->sendRequest('POST', $requestUrl, compact('customerReferenceNumber'));
+    }
+
+    /**
+     * getInterBankInquiry
+     *
+     * @param  array $fields
+     * @return \Aslam\Bni\Response
+     */
+    public function getInterBankInquiry(array $fields)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/getinterbankinquiry';
+        return $this->sendRequest('POST', $requestUrl, $fields);
+    }
+
+    /**
+     * getInterBankPayment
+     *
+     * @param  array $fields
+     * @return \Aslam\Bni\Response
+     */
+    public function getInterBankPayment(array $fields)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/getinterbankpayment';
+        return $this->sendRequest('POST', $requestUrl, $fields);
+    }
+
+    /**
+     * holdAmount
+     *
+     * @param  array $fields
+     * @return \Aslam\Bni\Response
+     */
+    public function holdAmount(array $fields)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/holdamount';
+        return $this->sendRequest('POST', $requestUrl, $fields);
+    }
+
+    /**
+     * holdAmountRelease
+     *
+     * @param  array $fields
+     * @return \Aslam\Bni\Response
+     */
+    public function holdAmountRelease(array $fields)
+    {
+        $requestUrl = $this->apiUrl . '/H2H/v2/holdamountrelease';
+        return $this->sendRequest('POST', $requestUrl, $fields);
     }
 }
